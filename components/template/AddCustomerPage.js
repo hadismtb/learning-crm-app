cf2c011619668bcd2f69f887c8fe389ab85e4cb8
Hadis <hadisematloob@yahoo.com>
2025-12-14 17:41:16 +0330

    feat(add-customer): use form module and put action buttons

--- components/template/AddCustomerPage.js
+++ components/template/AddCustomerPage.js
@@ -1,5 +1,36 @@
+import { useState } from "react";
+import Form from "@/components/module/Form";
+
 function AddCustomerPage() {
-  return <div></div>;
+  const [form, setForm] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    address: "",
+    phone: "",
+    postalCode: "",
+    date: "",
+    products: [],
+  });
+
+  const cancelHandler = () => {};
+
+  const saveHandler = () => {};
+
+  return (
+    <div className="customer-page">
+      <h4>Add New Customer</h4>
+      <Form form={form} setForm={setForm} />
+      <div className="customer-page__buttons">
+        <button className="first" onClick={cancelHandler}>
+          Cancel
+        </button>
+        <button className="second" onClick={saveHandler}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default AddCustomerPage;
