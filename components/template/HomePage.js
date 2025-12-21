0172b7a64209da19ee602b50d979494b7bd9e172
Hadis <hadisematloob@yahoo.com>
2025-12-21 06:43:25 +0330

    style(home-page): use card component to display customers info

--- components/template/HomePage.js
+++ components/template/HomePage.js
@@ -1,6 +1,13 @@
+import Card from "@/components/module/Card";
+
 function HomePage({ customers }) {
-  console.log(customers);
-  return <div>Customers</div>;
+  return (
+    <div>
+      {customers?.map((customer) => (
+        <Card key={customer._id} customer={customer} />
+      ))}
+    </div>
+  );
 }
 
 export default HomePage;
