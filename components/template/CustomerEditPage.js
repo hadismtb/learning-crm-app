3ddaf1ec67583de9d4663b0a6d883786b360f938
Hadis <hadisematloob@yahoo.com>
2025-12-28 05:52:14 +0330

    feat(edit-page): develop edit form and handle edit functionality

--- components/template/CustomerEditPage.js
+++ components/template/CustomerEditPage.js
@@ -1,6 +1,48 @@
+import { useState } from "react";
+import Form from "@/components/module/Form";
+import { useRouter } from "next/router";
+
 function CustomerEditPage({ data, id }) {
-  console.log(data, id);
-  return <div>edit page</div>;
+  const [form, setForm] = useState({
+    name: data.name,
+    email: data.email,
+    lastName: data.lastName,
+    phone: data.phone || "",
+    address: data.address || "",
+    postalCode: data.postalCode || "",
+    products: data.products || [],
+    date: data.date || "",
+  });
+
+  const router = useRouter();
+
+  const cancelHandler = () => router.push("/");
+
+  const saveHandler = async () => {
+    const res = await fetch(`/api/edit/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify({ data: form }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    if (data.status === "success") return router.push("/");
+  };
+
+  return (
+    <div className="customer-page">
+      <h4>Edit Customer</h4>
+      <Form form={form} setForm={setForm} />
+
+      <div className="customer-page__buttons">
+        <button className="first" onClick={cancelHandler}>
+          Cancel
+        </button>
+        <button className="second" onClick={saveHandler}>
+          Edit
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default CustomerEditPage;
