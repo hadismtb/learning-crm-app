ef7ca30890e7245f109ce9bc487135a081a276c6
Hadis <hadisematloob@yahoo.com>
2025-12-31 05:58:55 +0330

    feat(login): create login route

---
+++ pages/login.js
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <div style={{ color: "white" }}>
+      <h3>Login Form</h3>
+    </div>
+  );
+}
+
+export default Login;
--- utils/auth.js
+++ utils/auth.js
@@ -3,8 +3,6 @@ import { hash } from "bcryptjs";
 async function hashPassword(password) {
   const hashedPassword = await hash(password, 12);
 
-  console.log(hashedPassword);
-
   return hashedPassword;
 }
 
