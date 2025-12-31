3496d206c7ea5b93cbafdc65b963485c16f3607d
Hadis <hadisematloob@yahoo.com>
2025-12-31 05:47:31 +0330

    feat(signup): develop signup form

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,32 @@
+import { useState } from "react";
+
 function Signup() {
-  return <div style={{ color: "white" }}>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signupHandler = async (event) => {};
+
+  return (
+    <div style={{ color: "white" }}>
+      <h3>Signup Form</h3>
+
+      <div>
+        <input
+          placeholder="Email"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button onClick={signupHandler}>Sign Up</button>
+    </div>
+  );
 }
 
 export default Signup;
