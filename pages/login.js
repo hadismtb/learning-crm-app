bb46dbb08038f48353431758ff1246b47a26830e
Hadis <hadisematloob@yahoo.com>
2025-12-31 11:49:47 +0330

    feat(login-form): develop login form

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,43 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const loginHandler = async (event) => {
+    const response = await fetch("/api/auth/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await response.json();
+    if (data.status === "success") {
+      return router.push("/");
+    }
+  };
+
   return (
     <div style={{ color: "white" }}>
       <h3>Login Form</h3>
+
+      <div>
+        <input
+          placeholder="Email"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button onClick={loginHandler}>Login</button>
     </div>
   );
 }
