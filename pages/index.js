dfbf8a2db3370fdbf3ec23a6066cbacc84df0da1
Hadis <hadisematloob@yahoo.com>
2025-12-31 15:16:21 +0330

    feat(server-protect-auth-pages): protect main page from server

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,35 @@
 import connectDB from "@/utils/connectDB";
 import Customer from "@/models/Customer";
 import HomePage from "@/components/template/HomePage";
+import { verifyToken } from "@/utils/auth";
 
 export default function Index({ customers }) {
   return <HomePage customers={customers} />;
 }
 
-export async function getServerSideProps() {
-  try {
-    await connectDB();
-    const customers = await Customer.find();
+export async function getServerSideProps(context) {
+  const { token } = context.req.cookies;
+  const secretKey = process.env.SECRET_KEY;
+
+  const result = verifyToken(token, secretKey);
+
+  if (!result) {
     return {
-      props: { customers: JSON.parse(JSON.stringify(customers)) },
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
     };
-  } catch (error) {
-    return {
-      notFound: true,
-    };
-  }
+  } else
+    try {
+      await connectDB();
+      const customers = await Customer.find();
+      return {
+        props: { customers: JSON.parse(JSON.stringify(customers)) },
+      };
+    } catch (error) {
+      return {
+        notFound: true,
+      };
+    }
 }
