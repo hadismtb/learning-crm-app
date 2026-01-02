51a36dca45fa6b91b0ce2a08a516f066ed24922c
Hadis <hadisematloob@yahoo.com>
2026-01-02 18:05:48 +0330

    feat(logout-api): create an api for logout

---
+++ pages/api/auth/logout.js
@@ -0,0 +1,15 @@
+import { serialize } from "cookie";
+
+async function handler(req, res) {
+  if (req.method !== "GET") return;
+
+  const serialized = serialize("token", "", {
+    path: "/",
+    maxAge: 0,
+  });
+
+  res
+    .status(200)
+    .setHeader("Set-Cookie", serialized)
+    .json({ status: "success", message: "Logged out successfully" });
+}
