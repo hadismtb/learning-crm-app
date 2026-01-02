0ef449531bda40e0972d4b6ff50a803698ba8306
Hadis <hadisematloob@yahoo.com>
2026-01-02 18:31:53 +0330

    feat(auth-options): display/hide authentication options based on the login status

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,31 +1,54 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 function Layout({ children }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   const router = useRouter();
+
   const logoutHandler = async () => {
     const res = await fetch("/api/auth/logout");
 
     const data = await res.json();
 
     if (data.status === "success") {
+      setIsLoggedIn(false);
       return router.push("/login");
     }
   };
+
+  useEffect(() => {
+    fetch("/api/user")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === "success") {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+      });
+  }, []);
 
   return (
     <>
       <header className="header">
         <h2>Hamim CRM</h2>
-        <Link href="/add-customer">Add Customer</Link>
+        {isLoggedIn && <Link href="/add-customer">Add Customer</Link>}
         <div>
-          <Link href="/signup">Signup</Link>
-          <Link href="/login" style={{ marginLeft: "8px" }}>
-            Login
-          </Link>
-          <button style={{ marginLeft: "8px" }} onClick={logoutHandler}>
-            Logout
-          </button>
+          {!isLoggedIn && (
+            <>
+              <Link href="/signup">Signup</Link>
+              <Link href="/login" style={{ marginLeft: "8px" }}>
+                Login
+              </Link>
+            </>
+          )}
+          {isLoggedIn && (
+            <button style={{ marginLeft: "8px" }} onClick={logoutHandler}>
+              Logout
+            </button>
+          )}
         </div>
       </header>
       <div className="main">{children}</div>
